9899bcdc10d49082bb4b509236d2b87e08fe6476
OnhkimKilav <mihno.valik@mail.ru>
2018-03-14 00:34:27 +0300

    home task

--- src/lesson10/homeTask/Customer.java
+++ src/lesson10/homeTask/Customer.java
@@ -1,5 +1,7 @@
 package lesson10.homeTask;
 
+import java.util.Date;
+
 /**
  * Created by Valik on 12.03.2018.
  */
@@ -7,6 +9,12 @@ public class Customer {
     private String name;
     private String city;
     private String gender;
+
+    public Customer(String name, String city, String gender) {
+        this.name = name;
+        this.city = city;
+        this.gender = gender;
+    }
 
     public String getName() {
         return name;
--- src/lesson10/homeTask/ElectronicsOrder.java
+++ src/lesson10/homeTask/ElectronicsOrder.java
@@ -1,24 +1,42 @@
 package lesson10.homeTask;
 
+        import java.util.Date;
+
 /**
  * Created by Valik on 12.03.2018.
  */
 public class ElectronicsOrder extends Order {
     private int guaranteeMonths;
 
+    public ElectronicsOrder(String itemName, Date dateCreated, String shipFromCity, String shipToCity, int basePrice, Customer customerOwned) {
+        super(itemName, dateCreated, shipFromCity, shipToCity, basePrice, customerOwned);
+    }
+
     private String[] canShippingFromAndToCity = {"Киев", "Одесса", "Днепр", "Харьков"};
+
+
 
     @Override
     void validateOrder() {
         for (String city : canShippingFromAndToCity) {
-            if (getShipFromCity() == city && getShipToCity() == city) {
-                getShipFromCity();
-                getShipToCity();
+            if (getShipFromCity() == city && getShipToCity() == city && getBasePrice() > 100) {
+                setDateConfirmed(new Date());
             }
         }
-        if(getBasePrice() > 100)
-            getBasePrice();
+    }
 
+    @Override
+    void calculatePrice() {
+        double sumForDelivery = 0;
+        for(String city : canShippingFromAndToCity){
+            if(city == "Киев" || city == "Одесса"){
+                sumForDelivery = getBasePrice() - (getBasePrice() * 0.15);
+            }
+            sumForDelivery = getBasePrice() - (getBasePrice() * 0.1);
+        }
+        if(getBasePrice()>1000)
+            sumForDelivery -= sumForDelivery * 0.05;
 
+        setTotalPrice(sumForDelivery);
     }
 }
