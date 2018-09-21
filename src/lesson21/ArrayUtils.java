7b4a06d636832cda762bbb903fd101b78b83317c
OnhkimKilav <mihno.valik@mail.ru>
2018-09-21 11:18:18 +0300

    home task

---
+++ src/lesson21/ArrayUtils.java
@@ -0,0 +1,27 @@
+package lesson21;
+
+/**
+ * Created by Valik on 20.09.2018.
+ */
+public class ArrayUtils {
+
+    public static int maxElement(int[] array){
+
+        return 0;
+    }
+
+    public static int mimElement(int[] array){
+
+        return 0;
+    }
+
+    public static int sumOfElements(int[] array){
+
+        return 0;
+    }
+
+    public static int reverse(int[] array){
+
+        return 0;
+    }
+}
---
+++ src/lesson21/Company.java
@@ -0,0 +1,57 @@
+package lesson21;
+
+/**
+ * Created by Valik on 20.09.2018.
+ */
+public class Company {
+
+    private int numberOfEmployees;
+    private String name;
+
+    private static String licence;
+
+    private static int maxNumberOfEmployees = 100;
+
+
+    static {
+        System.out.println("Code block is called");
+        init();
+    }
+
+    public Company(int numberOfEmployees, String name) throws Exception {
+        if(numberOfEmployees > maxNumberOfEmployees)
+            throw new Exception("Company can have max " + maxNumberOfEmployees + " employees");
+
+        this.numberOfEmployees = numberOfEmployees;
+        this.name = name;
+    }
+
+    private static void init(){
+        maxNumberOfEmployees = 100;
+    }
+
+    public static void validate() throws Exception {
+        if(!licence.equals("TTT111"))
+            throw new Exception("Wrong licence " + licence);
+
+    }
+    public static void setLicence(String licence) {
+        Company.licence = licence;
+    }
+
+    public int getNumberofEmployees() {
+        return numberOfEmployees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLicence() {
+        return licence;
+    }
+
+//    private static class Test{
+//
+//    }
+}
