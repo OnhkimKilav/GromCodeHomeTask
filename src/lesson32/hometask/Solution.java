bb25f5c566563f8faca3d382a140d992f65c9bcf
OnhkimKilav <mihno.valik@mail.ru>
2018-10-23 11:01:53 +0300

    home task

--- src/lesson32/hometask/Solution.java
+++ src/lesson32/hometask/Solution.java
@@ -15,24 +15,18 @@ public class Solution {
         int sum = 0;
 
         while (countWrong >= 0) {
-            boolean point = true;
             String str = reader.readLine();
             String[] strings = str.split(" ");
-            validateLength(strings.length);
+            int index = 0;
 
             for (String string : strings) {
-                if(!validateStr(string)){
-                    point = false;
+                int number = validate(string, strings.length);
+                if(number == 0)
                     break;
-                }
-                int iStr = Integer.parseInt(string);
-                if (!validateInt(iStr)) {
-                    point = false;
-                    break;
-                }
-                sum += iStr;
+                sum += number;
+                index++;
             }
-            if (point || countWrong == 0)
+            if (index == strings.length || countWrong == 0)
                 break;
         }
         if (countWrong == 0) {
@@ -43,42 +37,27 @@ public class Solution {
         System.out.println(sum);
     }
 
-    private static boolean validateLength(int size){
-        if(!(size == 10)){
+    private static int validate(String str, int strLength) {
+        if (!(str.matches("[\\d]+")) || !(strLength == 10)) {
             if (countWrong == 1) {
                 countWrong--;
-                return false;
+                return 0;
             }
             countWrong--;
             System.out.println("You numbers are wrong. You have " + countWrong + " attempts to try again");
-            return false;
+            return 0;
         }
-        return true;
-    }
-
-    private static boolean validateStr(String str){
-        if(!str.matches("[\\d]+")){
-            if (countWrong == 1) {
-                countWrong--;
-                return false;
-            }
-            countWrong--;
-            System.out.println("You numbers are wrong. You have " + countWrong + " attempts to try again");
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean validateInt(int iStr) throws IOException {
+        int iStr = Integer.parseInt(str);
         if (iStr > 100) {
             if (countWrong == 1) {
                 countWrong--;
-                return false;
+                return 0;
             }
             countWrong--;
             System.out.println("You numbers are wrong. You have " + countWrong + " attempts to try again");
-            return false;
+            return 0;
         }
-        return true;
+
+        return iStr;
     }
 }
