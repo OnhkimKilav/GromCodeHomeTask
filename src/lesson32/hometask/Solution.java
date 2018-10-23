3023f123f1eb91429a81985949563628780891a8
OnhkimKilav <mihno.valik@mail.ru>
2018-10-23 15:47:56 +0300

    home task

--- src/lesson32/hometask/Solution.java
+++ src/lesson32/hometask/Solution.java
@@ -13,20 +13,21 @@ public class Solution {
     public static void readNumber() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int sum = 0;
+        int index;
 
-        while (countWrong >= 0) {
+        while (countWrong > 0) {
             String str = reader.readLine();
             String[] strings = str.split(" ");
-            int index = 0;
+            index = 0;
 
             for (String string : strings) {
                 int number = validate(string, strings.length);
-                if(number == 0)
+                if(number == -1)
                     break;
                 sum += number;
                 index++;
             }
-            if (index == strings.length || countWrong == 0)
+            if (index == strings.length )
                 break;
         }
         if (countWrong == 0) {
@@ -38,24 +39,24 @@ public class Solution {
     }
 
     private static int validate(String str, int strLength) {
-        if (!(str.matches("[\\d]+")) || !(strLength == 10)) {
+        if (!str.matches("[\\d]+") || !(strLength == 10)) {
             if (countWrong == 1) {
                 countWrong--;
-                return 0;
+                return -1;
             }
             countWrong--;
             System.out.println("You numbers are wrong. You have " + countWrong + " attempts to try again");
-            return 0;
+            return -1;
         }
         int iStr = Integer.parseInt(str);
         if (iStr > 100) {
             if (countWrong == 1) {
                 countWrong--;
-                return 0;
+                return -1;
             }
             countWrong--;
             System.out.println("You numbers are wrong. You have " + countWrong + " attempts to try again");
-            return 0;
+            return -1;
         }
 
         return iStr;
