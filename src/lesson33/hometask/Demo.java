6278604f3188a1ea69a2ad391cc2a5a86d3c59e4
OnhkimKilav <mihno.valik@mail.ru>
2018-10-26 13:10:07 +0300

    home task

--- src/lesson33/hometask/Demo.java
+++ src/lesson33/hometask/Demo.java
@@ -1,10 +1,12 @@
 package lesson33.hometask;
 
+import java.io.FileNotFoundException;
+
 /**
  * Created by Valik on 25.10.2018.
  */
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Solution.writeToFileFromConsole("D:/Programs/YandexDisk/Программач Java/какие то файлы/test1.txt");
     }
 }
--- src/lesson33/hometask/Solution.java
+++ src/lesson33/hometask/Solution.java
@@ -3,30 +3,28 @@ package lesson33.hometask;
 import org.apache.commons.io.IOUtils;
 
 import java.io.*;
-import java.util.ArrayList;
 
 /**
  * Created by Valik on 25.10.2018.
  */
 public class Solution {
-    public static void writeToFileFromConsole(String path) {
+    public static void writeToFileFromConsole(String path) throws FileNotFoundException {
+        if (!new File(path).canRead())
+            throw new FileNotFoundException("File with path " + path + " not found");
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = null;
-        String message = null;
-        ArrayList<String> list = new ArrayList<>();
+        String message;
+
         System.out.println("Enter file content to write in the file");
         try {
             writer = new BufferedWriter(new FileWriter(path, true));
             while (!(message = reader.readLine()).equals("wr")) {
-                try {
-                    writer.write(message);
-                    writer.append('\n');
-                } catch (IOException e) {
-                    System.out.println("Can't write to file with path " + path);
-                }
+                writer.write(message);
+                writer.append("\r\n");
             }
         } catch (IOException e) {
-            System.out.println("File with path " + path + " not found");
+            System.out.println("Can't write to file with path " + path);
         } finally {
             IOUtils.closeQuietly(reader);
             IOUtils.closeQuietly(writer);
