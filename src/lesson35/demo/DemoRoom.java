bafea7062809ae1e2c90179887fb5b7ea215f5a9
OnhkimKilav <mihno.valik@mail.ru>
2019-01-08 14:42:19 +0300

    home task

--- src/lesson35/demo/DemoRoom.java
+++ src/lesson35/demo/DemoRoom.java
@@ -12,7 +12,7 @@ import java.util.Date;
  * Created by Valik on 05.11.2018.
  */
 public class DemoRoom {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws Exception {
         RoomDAO roomDAO = new RoomDAO();
         RoomService roomService = new RoomService();
         Room room = new Room(2, 1500, true, false, new Date(), new Hotel("Ug", "Ukraine", "Kiev", "test"));
